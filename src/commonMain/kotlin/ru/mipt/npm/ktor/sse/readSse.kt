3672e57e8c1c3c8c452433151c4d4fdbbe3b1bbf
Alexander Nozik <altavir@gmail.com>
2020-10-21 15:12:53 +0300

    Catch non-cancelation errors

--- src/commonMain/kotlin/ru/mipt/npm/ktor/sse/readSse.kt
+++ src/commonMain/kotlin/ru/mipt/npm/ktor/sse/readSse.kt
@@ -10,12 +10,9 @@ import io.ktor.utils.io.ByteReadChannel
 import io.ktor.utils.io.ByteWriteChannel
 import io.ktor.utils.io.readUTF8Line
 import io.ktor.utils.io.writeStringUtf8
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
 
 
 /**
@@ -41,54 +38,62 @@ public fun HttpClient.readSse(address: String, block: suspend (SseEvent) -> Unit
     var reconnectDelay: Long = 3000
     var lastEventId: String? = null
     while (isActive) {
-        get<HttpStatement>(address) {
-            this.header("Accept-Encoding", "")
-            this.header("Accept", "text/event-stream")
-            this.header("Cache-Control", "no-cache")
+        try {
+            get<HttpStatement>(address) {
+                this.header("Accept-Encoding", "")
+                this.header("Accept", "text/event-stream")
+                this.header("Cache-Control", "no-cache")
 
-            if (lastEventId != null) {
-                header("Last-Event-Id", lastEventId);
-            }
-        }.execute { response: HttpResponse ->
-            // Response is not downloaded here.
-            val channel = response.receive<ByteReadChannel>()
-            while (isActive) {
-                //val lines = ArrayList<String>()
-                val builder = StringBuilder()
-                var id: String? = null
-                var event: String? = null
-                var retryValue: String? = null
-                //read lines until blank line or the end of stream
+                if (lastEventId != null) {
+                    header("Last-Event-Id", lastEventId);
+                }
+            }.execute { response: HttpResponse ->
+                // Response is not downloaded here.
+                val channel = response.receive<ByteReadChannel>()
+                while (isActive) {
+                    //val lines = ArrayList<String>()
+                    val builder = StringBuilder()
+                    var id: String? = null
+                    var event: String? = null
+                    var retryValue: String? = null
+                    //read lines until blank line or the end of stream
 
-                do {
-                    val line = channel.readUTF8Line()
-                    if (line != null && line.isNotBlank()) {
-                        val colonIndex = line.indexOf(": ")
-                        val key = line.substring(0 until colonIndex)
-                        val value = line.substring(colonIndex)
-                        when (key) {
-                            "id" -> {
-                                id = value
-                                lastEventId = value
+                    do {
+                        val line = channel.readUTF8Line()
+                        if (line != null && line.isNotBlank()) {
+                            val colonIndex = line.indexOf(": ")
+                            val key = line.substring(0 until colonIndex)
+                            val value = line.substring(colonIndex)
+                            when (key) {
+                                "id" -> {
+                                    id = value
+                                    lastEventId = value
+                                }
+                                "event" -> event = value
+                                "data" -> builder.append(value)
+                                "retry" -> retryValue = value
+                                else -> error("Unrecognized event-stream key $key")
                             }
-                            "event" -> event = value
-                            "data" -> builder.append(value)
-                            "retry" -> retryValue = value
-                            else -> error("Unrecognized event-stream key $key")
                         }
-                    }
-                } while (line?.isBlank() != true)
+                    } while (line?.isBlank() != true)
 
-                if (retryValue != null) {
-                    val delayTime = retryValue.toLong()
-                    check(delayTime>0){"SSE reconnect value must be positive"}
-                    reconnectDelay = delayTime
-                } else if (builder.isNotBlank()) {
-                    block(SseEvent(builder.toString(), event, id))
+                    if (retryValue != null) {
+                        val delayTime = retryValue.toLong()
+                        check(delayTime > 0) { "SSE reconnect value must be positive" }
+                        reconnectDelay = delayTime
+                    } else if (builder.isNotBlank()) {
+                        block(SseEvent(builder.toString(), event, id))
+                    }
                 }
             }
+            delay(reconnectDelay)
+        } catch (t: Throwable) {
+            //TODO add logging here
+            if(t is CancellationException){
+                //finish reading on cancellation
+                throw t
+            }
         }
-        delay(reconnectDelay)
     }
 }
 
